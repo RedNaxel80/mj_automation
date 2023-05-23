a5539f35166cc92f7acd3e7ed75e79dc27cdf965
rednaxel80 <atlassian!(Rednaksela80)>
2023-05-23 11:05:48 +0200

    encapsulated loop commands to bot

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ def start_bot(bot):
     bot.start_bot()
 
 
+def mj_command(loop, command):
+    return asyncio.run_coroutine_threadsafe(command, loop)
+
+
 mj_bot = MjAutomator()
 thread = threading.Thread(target=start_bot, args=(mj_bot,))
 thread.start()
 
 time.sleep(10)
 
-loop = mj_bot.client.loop
-future = asyncio.run_coroutine_threadsafe(mj_bot.prompter.start_prompting(), loop)
+mj_loop = mj_bot.client.loop
+future = mj_command(mj_loop, mj_bot.prompter.start_prompting())
+
+# future = asyncio.run_coroutine_threadsafe(mj_bot.prompter.start_prompting(), loop)
 # future = asyncio.run_coroutine_threadsafe(mj_bot.prompter.send_prompt("laboratory"), loop)
