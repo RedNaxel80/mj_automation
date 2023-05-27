749c7ec3c8c3e668b23c56d95f5f9408cbe95d51
rednaxel80 <atlassian!(Rednaksela80)>
2023-05-27 10:08:49 +0200

    (technical) separated uploader and logger classes

--- mj_automation.py
+++ mj_automation.py
@@ -15,13 +15,8 @@ import queue
 
 
 class MjAutomator:
-    def __init__(self, server_id=config.SERVER_ID, channel_id=config.CHANNEL_ID, auto_run=False):
-        self.guild_id = server_id
-        self.channel_id = channel_id
+    def __init__(self, auto_run=False):
         self.prompt_counter = 0
-        self.directory = os.getcwd()
-        self.prompt_file = open(config.PROMPT_FILE, 'r')
-        self.done_prompt_file = open(config.DONE_PROMPT_FILE, 'a')
         self.auto_run = auto_run
         self.log_started = False
         self.message_counter = -1  # it will get to zero with bot start message
@@ -30,12 +25,15 @@ class MjAutomator:
         self.intents = discord.Intents.all()
         self.intents.message_content = True
         self.client = commands.Bot(command_prefix='$', intents=self.intents)  # the actual bot
-        self.guild = None
-        self.channel = None
-        self.downloader = self.Downloader(self) if config.ENABLE_DOWNLOAD else None  # the downloader class
-        self.prompter = self.Prompter(self) if config.ENABLE_PROMPTING else None  # the prompter class
-        self.mj_imagine, self.mj_upscale, self.mj_upscale_max = self.discord_setup()  # bringing the inner methods to the main class
+        self.guild = None  # set after bot is ready
+        self.channel = None  # set after bot is ready
+
+        self.downloader = self.Downloader(self)  # the downloader class
+        self.prompter = self.Prompter(self)  # the prompter class
+        self.upscaler = self.Upscaler(self)  # the upscaler class
+        self.logger = self.Logger(self)  # the logger class
         self.job_manager = self.JobManager(self)  # the job manager class, handling pool of all midjourney commmands
+        self.mj_imagine, self.mj_upscale, self.mj_upscale_max = self.discord_setup()  # bringing the inner methods to the main class
 
         if self.auto_run:
             self.start_bot()
@@ -49,10 +47,6 @@ class MjAutomator:
     def discord_setup(self):
         @self.client.event
         async def on_ready():
-            # flush the log file if it's not persistent
-            if config.LOG_ENABLED and not config.LOG_PERSISTENT and os.path.exists(config.LOG_FILE):
-                os.remove(config.LOG_FILE)
-
             # Get a reference to the guild (server) and channel
             self.guild = self.client.get_guild(config.SERVER_ID)
             self.channel = self.guild.get_channel(config.CHANNEL_ID)
@@ -61,30 +55,26 @@ class MjAutomator:
             print("--run settings--")
             print(f"Downloader: {'enabled' if self.downloader else 'disabled'}, prompter: {'enabled' if self.prompter else 'disabled'}")
             print(f"Upscale: {config.ENABLE_UPSCALE}, upscale_max: {config.ENABLE_UPSCALE_MAX}")
-            print(f"Download original{config.DOWNLOAD_ORIGINAL}, download upscale: {config.DOWNLOAD_UPSCALE}, download upscaled_max: {config.DOWNLOAD_UPSCALE_MAX}")
+            print(f"Download original: {config.DOWNLOAD_ORIGINAL}, download upscale: {config.DOWNLOAD_UPSCALE}, download upscale_max: {config.DOWNLOAD_UPSCALE_MAX}")
             print("---------------")
 
-            asyncio.create_task(self.job_manager.process_jobs())
             await self.channel.send("Bot ready!")
-            if self.prompter and self.auto_run:
-                asyncio.create_task(self.prompter.start_prompting())
+            asyncio.create_task(self.job_manager.process_jobs())
+            asyncio.create_task(self.prompter.prompt_process())
 
         @self.client.event
         async def on_message(message):
-            self.message_counter += 1
             # get all messsages from the channel and log them
-            if config.LOG_ENABLED:
-                with open(config.LOG_FILE, 'a') as log_file:
-                    log_file.write(f"{datetime.now()}: {message.author.id} {message.content}\n")
+            await self.logger.log(f"{message.author.id}: {message.content}")
 
             # if the message is not from midjourney itself, ignore it
             if message.author.id != config.MIDJOURNEY_ID or message.content == "" or not message.attachments:
                 return
 
-            if self.downloader:
-                await self.downloader.download_process(message)
-            if self.prompter:
-                await self.prompter.upscale_process(message)
+            # process the message
+            self.message_counter += 1
+            await self.downloader.download_process(message)
+            await self.upscaler.upscale_process(message)
 
         @self.client.command()
         async def mj_imagine(ctx, *, prompt: str):
@@ -104,8 +94,11 @@ class MjAutomator:
             response = mj_commands.upscale(index, message_id, message_hash)
 
             if response.status_code >= 400:
-                await ctx.send("Upscale: Request has failed; please try later")
+                await self.logger.log(f"Upscale ERROR: Request has failed; please try later, id: {message_id}, hash: {message_hash}")
+                await ctx.send(f"Upscale: Request has failed; please try later")
                 return
+            else:
+                await self.logger.log(f"Upscale SUCCESS: id: {message_id}, hash: {message_hash}")
 
         @self.client.command()
         async def mj_upscale_max(ctx, message_id: str, message_hash: str):
@@ -125,8 +118,12 @@ class MjAutomator:
         def __init__(self, main):
             self.main = main
             self.client = self.main.client
+            self.directory = os.getcwd()
 
         async def download_process(self, message):
+            if not config.ENABLE_DOWNLOAD:
+                return
+
             # different mj versions have different message formats, so we need to check for a list here
             file_prefix = config.UPSCALE_PREFIX if any(s in message.content.lower() for s in config.UPSCALE_TAGS) else ''
             # new condition for upscale max
@@ -155,7 +152,7 @@ class MjAutomator:
                 if not os.path.exists(input_folder):
                     os.makedirs(input_folder)
 
-                with open(f"{self.main.directory}/{input_folder}/{filename}", "wb") as f:
+                with open(f"{self.directory}/{input_folder}/{filename}", "wb") as f:
                     f.write(response.content)
                 # print(f"Image downloaded: {filename}")
 
@@ -172,13 +169,13 @@ class MjAutomator:
                     bottom_right.save(
                         os.path.join(output_folder, config.SPLIT_PREFIX + file_prefix + "_bottom_right.jpg"))
                     # Delete the input file
-                    os.remove(f"{self.main.directory}/{input_folder}/{filename}")
+                    os.remove(f"{self.directory}/{input_folder}/{filename}")
 
                 else:
-                    output_path = f"{self.main.directory}/" if not config.DOWNLOAD_ABSOLUTE_PATH else ""
+                    output_path = f"{self.directory}/" if not config.DOWNLOAD_ABSOLUTE_PATH else ""
                     output_path += f"{output_folder}/{filename}"
 
-                    os.rename(f"{self.main.directory}/{input_folder}/{filename}", output_path)
+                    os.rename(f"{self.directory}/{input_folder}/{filename}", output_path)
 
         def split_image(self, image_file):
             with Image.open(image_file) as im:
@@ -200,18 +197,33 @@ class MjAutomator:
             self.main = main
             self.client = self.main.client
 
-        async def start_prompting(self):
-            prompts = self.main.prompt_file.readlines()
+        async def prompt_process(self):
+            if not config.ENABLE_PROMPTING:
+                return
 
-            for prompt in prompts:
-                prompt = prompt.strip()
-                if prompt != "":
-                    await self.main.job_manager.add_job((self.main.prompter.send_prompt, prompt))
-            print('Prompt batch import finished. Created jobs:', await self.main.job_manager.get_job_count())
+            await self.get_prompts_from_file()
+
+        async def get_prompts_from_file(self):
+            try:
+                prompt_file = open(config.PROMPT_FILE, 'r')
+                done_prompt_file = open(config.DONE_PROMPT_FILE, 'a')
+                prompts = prompt_file.readlines()
+
+                for prompt in prompts:
+                    prompt = prompt.strip()
+                    if prompt != "":
+                        await self.main.job_manager.add_job((self.main.prompter.send_prompt, prompt))
+                print('Prompt batch import finished. Created jobs:', await self.main.job_manager.get_job_count())
+            except FileNotFoundError:
+                print("Error on reading the prompt file.")
+
+        async def get_prompt(self):
+            pass
 
         async def send_prompt(self, prompt):
-            if self.main.channel.last_message_id is None:
-                await self.main.channel.send("Context message for the bot's prompts - do not delete")
+            # if self.main.channel.last_message_id is None:
+            #     await self.main.channel.send("Context message for the bot's prompts - do not delete")
+            #     await asyncio.sleep(5)
 
             # Check if there are any messages in the channel
             if self.main.channel.last_message_id is not None:
@@ -220,6 +232,9 @@ class MjAutomator:
                     message = await self.main.channel.fetch_message(self.main.channel.last_message_id)
                 except discord.NotFound:
                     print("The last message in the channel was deleted.")
+                    await self.main.channel.send("Context message for the bot's prompts - do not delete")
+                    await asyncio.sleep(5)
+                    await self.send_prompt(prompt)
                     return
                 except discord.Forbidden:
                     print("The bot doesn't have permission to read message history in the channel.")
@@ -233,16 +248,22 @@ class MjAutomator:
             else:
                 print("There are no messages in the channel.")
 
+    class Upscaler:
+        def __init__(self, main):
+            self.main = main
+            self.client = self.main.client
+
         async def upscale_process(self, message):
+            if not config.ENABLE_UPSCALE and not config.ENABLE_UPSCALE_MAX:
+                return
+
             # if message.reference or not config.ENABLE_UPSCALE:  # if the message is a reply or auto upscale is disabled
             #     return
             if message.author.id != config.MIDJOURNEY_ID:
                 return
 
-            if not config.ENABLE_UPSCALE and not config.ENABLE_UPSCALE_MAX:
-                return
-
             try:
+                # if the message has an attachment, ge the hash from the filename
                 message_hash = str((message.attachments[0].url.split("_")[-1]).split(".")[0])
                 message_id = str(message.id)
                 ctx = await self.client.get_context(message)
@@ -251,15 +272,13 @@ class MjAutomator:
                 # choose upscaler based on message contents, but ignore already max_upscaled images
                 if any(s in lowered_message_content for s in config.UPSCALE_TAGS) and \
                         config.UPSCALE_MAX_TAG not in lowered_message_content and config.ENABLE_UPSCALE_MAX:
-                    # await self.max_upscale(ctx, message_id, message_hash)
                     # adding to job queue
-                    await self.main.job_manager.add_job((self.main.prompter.max_upscale, ctx, message_id, message_hash))
+                    await self.main.job_manager.add_job((self.main.upscaler.max_upscale, ctx, message_id, message_hash))
                 else:
                     # if it's already a max upscaled image, don't upscale it again as it causes errors
                     if config.UPSCALE_MAX_TAG not in lowered_message_content:
-                        # await self.default_upscale(ctx, message_id, message_hash)
                         # adding to job queue
-                        await self.main.job_manager.add_job((self.main.prompter.default_upscale, ctx, message_id, message_hash))
+                        await self.main.job_manager.add_job((self.main.upscaler.default_upscale, ctx, message_id, message_hash))
 
             except Exception as e:
                 print(f"An error occurred: {e}")
@@ -277,6 +296,7 @@ class MjAutomator:
             self.main = main
             self.client = self.main.client
             self.queue = asyncio.Queue()
+            self.job_number = 1
 
         async def add_job(self, job):
             await self.queue.put(job)
@@ -290,15 +310,35 @@ class MjAutomator:
 
         async def process_jobs(self):
             print("Starting job processing...")
+            await asyncio.sleep(10)
 
             while True:
                 job = await self.queue.get()
                 if job is not None:
                     try:
-                        await self.do_job(await self.queue.get())
+                        print(f"Processing job #{self.job_number}, jobs left in queue: {await self.get_job_count()}: {job}")
+                        self.job_number += 1
+                        await self.do_job(job)
                     except DiscordServerError as e:
                         print(f"DiscordServerError: {e}")
                         await asyncio.sleep(60)
 
                 await asyncio.sleep(30)  # sleep for 30 seconds between jobs (or waiting for next ones)
+
+    class Logger:
+        def __init__(self, main):
+            self.main = main
+            self.client = self.main.client
+
+            # flush the log file if it's not persistent
+            if os.path.exists(config.LOG_FILE) and not config.LOG_PERSISTENT:
+                os.remove(config.LOG_FILE)
+
+        async def log(self, message):
+            if not config.LOG_ENABLED:
+                return
+
+            with open(config.LOG_FILE, 'a') as log_file:
+                log_file.write(f"{datetime.now()}: {message}\n")
+
 
