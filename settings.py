05b484a51016bd37d4f66f4d6b45a0a3c50145fe
rednaxel80 <atlassian!(Rednaksela80)>
2023-06-08 13:47:00 +0200

    rewritten class - key:value switched and structure is now flat

--- settings.py
+++ settings.py
@@ -7,39 +7,45 @@ class Settings:
     # dictionary of json entries
     version = "version"
     setup_completed = "setup_completed"
-    gpt = "gpt"
-    api_key = "api_key"
-    master_query_file = "master_query_file"
-    discord = "discord"
-    server_id = "server_id"
-    channel_id = "channel_id"
-    bot_token = "bot_token"
-    main_token = "main_token"
-    api_url = "api_url"
-    mj_app_id = "mj_app_id"
-    download = "download"
-    enabled = "enabled"
-    folder = "folder"
-    use_absolute_path = "use_absolute_path"
-    original = "original"
-    upscale = "upscale"
-    upscale_max = "upscale_max"
-    split_original = "split_original"
-    split_prefix = "split_prefix"
-    upscale_max_prefix = "upscale_max_prefix"
-    upscale_prefix = "upscale_prefix"
-    allowed_extensions = "allowed_extensions"
-    prompt = "prompt"
-    file = "file"
-    done_file = "done_file"
-    upscale_tags = "upscale_tags"
-    upscale_max_tags = "upscale_max_tags"
-    logging = "logging"
-    persistent = "persistent"
-    jobmanager = "jobmanager"
-    timeout_between_jobs = "timeout_between_jobs"
-    hanged_job_timeout = "hanged_job_timeout"
-    concurrent_jobs_limit = "concurrent_jobs_limit"
+    config_initialized = "config_initialized"
+
+    gpt_api_key = "gpt_api_key"
+    gpt_master_query_file = "gpt_master_query_file"
+
+    discord_server_id = "discord_server_id"
+    discord_channel_id = "discord_channel_id"
+    discord_bot_token = "discord_bot_token"
+    discord_main_token = "discord_main_token"
+    discord_api_url = "discord_api_url"
+    discord_mj_app_id = "discord_mj_app_id"
+
+    download_enabled = "download_enabled"
+    download_folder = "download_folder"
+    download_folder_use_absolute_path = "download_folder_use_absolute_path"
+    download_upscale = "download_upscale"
+    download_upscale_max = "download_upscale_max"
+    download_original = "download_original"
+    download_split_original = "download_split_original"
+    download_allowed_extensions = "download_allowed_extensions"
+    download_split_prefix = "download_split_prefix"
+    download_upscale_max_prefix = "download_upscale_max_prefix"
+    download_upscale_prefix = "download_upscale_prefix"
+
+    prompt_enabled = "prompt_enabled"
+    prompt_enable_upscale = "prompter_enable_upscale"
+    prompt_enable_upscale_max = "prompter_enable_upscale_max"
+    prompt_file = "prompt_file"
+    prompt_done_file = "prompt_done_file"
+    prompt_upscale_tags = "prompt_upscale_tags"
+    prompt_upscale_max_tag = "prompt_upscale_max_tag"
+
+    logging_enabled = "logging_enabled"
+    logging_persistent = "logging_persistent"
+    logging_file = "logging_file"
+    
+    jobmanager_timeout_between_jobs = "jobmanager_timeout_between_jobs"
+    jobmanager_hanged_job_timeout = "jobmanager_hanged_job_timeout"
+    jobmanager_concurrent_jobs_limit = "jobmanager_concurrent_jobs_limit"
 
     def __init__(self, appname="Mj-auto", filename="settings.json"):
         # location of settings.json:
@@ -57,80 +63,85 @@ class Settings:
             os.makedirs(self.appdirs.user_config_dir)
         if not os.path.isfile(self.filename):
             self.create()
+        if not self.read(Settings.config_initialized):
             self.insert_defaults()
 
     def create(self):
         with open(self.filename, 'w') as f:
             json.dump({}, f)
 
-    def read(self, *keys):
+    def read(self, key):
         with open(self.filename, 'r') as f:
             data = json.load(f)
-        for key in keys:
-            data = data.get(key)
-        return data
+        return data.get(key)
 
-    def write(self, value, *keys):
+    def write(self, value, key):
         with open(self.filename, 'r') as f:
             data = json.load(f)
-        temp = data
-        for key in keys[:-1]:  # get to the second to last key
-            if key not in temp:
-                temp[key] = {}
-            temp = temp[key]
-        temp[keys[-1]] = value  # set the value for the last key
+        data[key] = value
+        with open(self.filename, 'w') as f:
+            json.dump(data, f)
+
+    def multi_read(self, *keys):
+        with open(self.filename, 'r') as f:
+            data = json.load(f)
+        return tuple(data.get(key) for key in keys)
+
+    def multi_write(self, **kwargs):
+        with open(self.filename, 'r') as f:
+            data = json.load(f)
+        for key, value in kwargs.items():
+            data[key] = value
         with open(self.filename, 'w') as f:
             json.dump(data, f)
 
     def insert_defaults(self):
         # app setup
-        self.write(0.1, Settings.version)
-        self.write(True, Settings.setup_completed)
+        self.write(Settings.version, 0.1)
+        self.write(Settings.config_initialized, True)
+        self.write(Settings.setup_completed, False)
 
         # gpt settings
-        self.write(None, Settings.gpt, Settings.api_key)
-        self.write(os.path.join(self.appdirs.user_config_dir, "gpt_query_file.txt"), Settings.gpt, Settings.master_query_file)
+        self.write(Settings.gpt_api_key, None)
+        self.write(Settings.gpt_master_query_file, os.path.join(self.appdirs.user_config_dir, "gpt_query_file.txt"))
 
         # discord settings
-        self.write(None, Settings.discord, Settings.server_id)  # id of the server the bot is supposed to run on
-        self.write(None, Settings.discord, Settings.channel_id)  # id of the channel the bot is supposed to *run prompts on* (the downloader will work for any channel it is a member of !!!)
-        self.write(None, Settings.discord, Settings.bot_token)  # token for the bot
-        self.write(None, Settings.discord, Settings.main_token)  # token for your discord account that has the midjourney paid subscription
+        self.write(Settings.discord_server_id, None)  # id of the server the bot is supposed to run on
+        self.write(Settings.discord_channel_id, None)  # id of the channel the bot is supposed to *run prompts on* (the downloader will work for any channel it is a member of !!!)
+        self.write(Settings.discord_bot_token, None)  # token for the bot
+        self.write(Settings.discord_main_token, None)  # token for your discord account that has the midjourney paid subscription
 
         # downloader settings
-        self.write(True, Settings.download, Settings.enabled)  # should the downloads be enabled?
-        self.write(os.path.join(os.path.expanduser("~"), "Downloads"), Settings.download, Settings.folder)  # download folder
-        self.write(True, Settings.download, Settings.use_absolute_path)  # is the above path an absolute one or relative to the script?
-        self.write(True, Settings.download, Settings.original)  # no point in downloading the original if you're upscaling all of them it anyway
-        self.write(True, Settings.download, Settings.upscale)
-        self.write(True, Settings.download, Settings.upscale_max)
-        self.write(True, Settings.download, Settings.split_original)  # do you want the original quadruple image split into four images?
-        self.write("S_", Settings.download, Settings.split_prefix)
-        self.write("U_", Settings.download, Settings.upscale_prefix)
-        self.write("UM_", Settings.download, Settings.upscale_max_prefix)
-        self.write([".png", ".jpg", ".jpeg", ".gif", ".webp"], Settings.download, Settings.allowed_extensions)  # do not touch
+        self.write(Settings.download_enabled, True)  # should the downloads be enabled?
+        self.write(Settings.download_folder, os.path.join(os.path.expanduser("~"), "Downloads"))  # download folder
+        self.write(Settings.download_folder_use_absolute_path, True)  # is the above path an absolute one or relative to the script?
+        self.write(Settings.download_original, True)  # no point in downloading the original if you're upscaling all of them it anyway
+        self.write(Settings.download_upscale, True)
+        self.write(Settings.download_upscale_max, True)
+        self.write(Settings.download_split_original, True)  # do you want the original quadruple image split into four images?
+        self.write(Settings.download_split_prefix, "S_")
+        self.write(Settings.download_upscale_prefix, "U_")
+        self.write(Settings.download_upscale_max_prefix, "UM_")
+        self.write(Settings.download_allowed_extensions, [".png", ".jpg", ".jpeg", ".gif", ".webp"])  # do not touch
 
         # prompter settings
-        self.write(True, Settings.prompt, Settings.enabled)  # should the prompting be enabled?
-        self.write(False, Settings.prompt, Settings.upscale)
-        self.write(False, Settings.prompt, Settings.upscale_max)  # this works only for v4 currently, it will create errors for v5 and v5.1
-        self.write(os.path.join(self.appdirs.user_config_dir, "mja_prompts.txt"), Settings.prompt, Settings.file)
-        self.write(os.path.join(self.appdirs.user_config_dir, "mja_prompts_completed.txt"), Settings.prompt, Settings.done_file)
-        self.write(["image #", "upscale"], Settings.prompt, Settings.upscale_tags)  # the part of the mj message that gets search for to consider it default upscale
-        self.write("upscaled (beta)", Settings.prompt, Settings.upscale_max_tags)  # as above but for upscale max
+        self.write(Settings.prompt_enabled, True)  # should the prompting be enabled?
+        self.write(Settings.prompt_enable_upscale, False)
+        self.write(Settings.prompt_enable_upscale_max, False)  # this works only for v4 currently, it will create errors for v5 and v5.1
+        self.write(Settings.prompt_file, os.path.join(self.appdirs.user_config_dir, "mja_prompts.txt"))
+        self.write(Settings.prompt_done_file, os.path.join(self.appdirs.user_config_dir, "mja_prompts_completed.txt"))
+        self.write(Settings.prompt_upscale_tags, ["image #", "upscale"])  # the part of the mj message that gets search for to consider it default upscale
+        self.write(Settings.prompt_upscale_max_tag, "upscaled (beta)")  # as above but for upscale max
 
         # logging settings
-        self.write(True, Settings.logging, Settings.enabled)
-        self.write(False, Settings.logging, Settings.persistent)
-        self.write(os.path.join(self.appdirs.user_config_dir, "mja_logs.txt"), Settings.logging, Settings.file)
+        self.write(Settings.logging_enabled, True)
+        self.write(Settings.logging_persistent, False)
+        self.write(Settings.logging_file, os.path.join(self.appdirs.user_config_dir, "mja_logs.txt"))
 
         # Technical settings - DO NOT EDIT THOSE:
-        self.write("https://discord.com/api/v10/interactions", Settings.discord, Settings.api_url)
-        self.write(936929561302675456, Settings.discord, Settings.mj_app_id)
-        self.write(4, Settings.jobmanager, Settings.timeout_between_jobs)  # timout between jobs in seconds, but should be a full divider of 60 (e.g. 2 & 5 are ok, but 7 is not)
-        self.write(300, Settings.jobmanager, Settings.hanged_job_timeout)  # hanged job timeout in seconds, but has to represent full minutes, otherwise it will be rounded down
-        self.write(5, Settings.jobmanager, Settings.concurrent_jobs_limit)  # concurrent jobs number has to stay way below maximum to avoid captcha checks, halts, and hangs
-
-
-config = Settings()
+        self.write(Settings.discord_api_url, "https://discord.com/api/v10/interactions")
+        self.write(Settings.discord_mj_app_id, 936929561302675456)
+        self.write(Settings.jobmanager_timeout_between_jobs, 4)  # timout between jobs in seconds, but should be a full divider of 60 (e.g. 2 & 5 are ok, but 7 is not)
+        self.write(Settings.jobmanager_hanged_job_timeout, 300)  # hanged job timeout in seconds, but has to represent full minutes, otherwise it will be rounded down
+        self.write(Settings.jobmanager_concurrent_jobs_limit, 5)  # concurrent jobs number has to stay way below maximum to avoid captcha checks, halts, and hangs
 
