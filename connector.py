49f307073ec238b3e3a01eeb0b370c4bff59f3c8
rednaxel80 <atlassian!(Rednaksela80)>
2023-06-17 00:02:58 +0200

    connector rewritten for expansions

---
+++ connector.py
@@ -0,0 +1,76 @@
+import asyncio
+import threading
+import time
+from ui import UI
+from mj_automation import MjAutomator
+
+
+class ConnectionType:
+    wxPython = 0
+    flask = 1
+    stdin = 2
+
+
+class Connector:
+    def __init__(self):
+        self.loop = None
+        self.bot = None
+        self.thread = None
+        self.conn = None
+        self.ui = None
+        self.conn_type = ConnectionType.wxPython
+        self.init_bot()
+        self.init_conn()
+        self.init_ui()
+
+    def __getattr__(self, name):
+        return getattr(self.conn, name)
+
+    def init_ui(self):
+        self.ui = UI(self.conn)
+
+    def init_bot(self):
+        self.bot = MjAutomator(auto_run=False) or None
+        self.thread = threading.Thread(target=self.bot.start_bot, args=())
+        self.thread.start()
+
+        # wait for the bot loop to be accessible
+        while not self.bot.ready_status:
+            time.sleep(1)
+        else:
+            self.loop = self.bot.client.loop
+
+    def init_conn(self):
+        if self.conn_type == ConnectionType.wxPython:
+            self.conn = ConnectorWx(self.bot, self.loop)
+        elif self.conn_type == ConnectionType.flask:
+            self.conn = ConnectorFlask(self.bot, self.loop)
+        # asyncio.run_coroutine_threadsafe(self.bot.prompter.send_prompt("parrot"), loop)  # test
+
+
+class ConnectorFlask:
+    def __init__(self, bot, loop):
+        self.loop = loop
+        self.bot = bot
+
+
+class ConnectorWx:
+    def __init__(self, bot, loop):
+        self.loop = loop
+        self.bot = bot
+
+    def send_command(self, command):
+        asyncio.run_coroutine_threadsafe(command, self.loop)
+
+    def send_prompt_to_bot(self, prompt):
+        # self.send_command(self.bot.prompter.send_prompt(prompt))
+        self.send_command(self.bot.prompter.parse_multiple_prompts(prompt))
+
+    def send_file_to_bot(self, file_path):
+        self.send_command(self.bot.prompter.get_prompts_from_file(file_path))
+
+    def start_bot(self):
+        pass
+
+    def stop_bot(self):
+        asyncio.run_coroutine_threadsafe(self.bot.stop_bot(), self.loop)
